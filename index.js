381af5a2dead1dbb580877cb92b5e302bf7c4947
irzooqi <yasirirzooqi@gmail.com>
2023-06-16 04:18:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution in this file!
+const employee = {
+    name: "Yasir Irzooqi",
+    streetAddress: "Millet Caddesi"
+
+};
+
+// non-destructive update
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   return {
+     ...employee,
+     [key]: value
+   };
+ }
+ 
+
+
+// destructive update 
+
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value)
+ {
+    employee[key] = value;
+    return employee;
+ }
+
+ // non-destructive function
+ function deleteFromEmployeeByKey(employee, key) {
+    
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+
+ }
+
+//destructive function
+
+ function destructivelyDeleteFromEmployeeByKey(employee,key) {
+    delete employee[key];
+    return employee;
+ }
+ 
+ // updates
+ 
+const updatedEmployee = updateEmployeeWithKeyandValue(employee, "position", "Manager");
+const copiedEmployee  = {...employee,position: "Manager" }
+const destructivelyupdatedEmployee = destructivelyUpdateEmployeeWithKeyandValue(employee, "position", "Manager");
